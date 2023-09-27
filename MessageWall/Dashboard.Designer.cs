6f2121dfef449a80c9fcb57ffe74e5725125264f
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-27 14:26:52 -0600

    demo complete.

--- MessageWall/Dashboard.Designer.cs
+++ MessageWall/Dashboard.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._lblMessage = new System.Windows.Forms.Label();
+            this._txtbMessage = new System.Windows.Forms.TextBox();
+            this._lstbMessages = new System.Windows.Forms.ListBox();
+            this._lblListBoxMessages = new System.Windows.Forms.Label();
+            this._btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _lblMessage
+            // 
+            this._lblMessage.AutoSize = true;
+            this._lblMessage.Location = new System.Drawing.Point(120, 62);
+            this._lblMessage.Name = "_lblMessage";
+            this._lblMessage.Size = new System.Drawing.Size(135, 36);
+            this._lblMessage.TabIndex = 0;
+            this._lblMessage.Text = "Message";
+            // 
+            // _txtbMessage
+            // 
+            this._txtbMessage.Location = new System.Drawing.Point(272, 57);
+            this._txtbMessage.Name = "_txtbMessage";
+            this._txtbMessage.Size = new System.Drawing.Size(444, 41);
+            this._txtbMessage.TabIndex = 1;
+            // 
+            // _lstbMessages
+            // 
+            this._lstbMessages.FormattingEnabled = true;
+            this._lstbMessages.ItemHeight = 36;
+            this._lstbMessages.Location = new System.Drawing.Point(126, 194);
+            this._lstbMessages.Name = "_lstbMessages";
+            this._lstbMessages.Size = new System.Drawing.Size(589, 508);
+            this._lstbMessages.TabIndex = 3;
+            // 
+            // _lblListBoxMessages
+            // 
+            this._lblListBoxMessages.AutoSize = true;
+            this._lblListBoxMessages.Location = new System.Drawing.Point(120, 155);
+            this._lblListBoxMessages.Name = "_lblListBoxMessages";
+            this._lblListBoxMessages.Size = new System.Drawing.Size(150, 36);
+            this._lblListBoxMessages.TabIndex = 3;
+            this._lblListBoxMessages.Text = "Messages";
+            // 
+            // _btnAdd
+            // 
+            this._btnAdd.Location = new System.Drawing.Point(774, 57);
+            this._btnAdd.Name = "_btnAdd";
+            this._btnAdd.Size = new System.Drawing.Size(154, 68);
+            this._btnAdd.TabIndex = 2;
+            this._btnAdd.Text = "Add";
+            this._btnAdd.UseVisualStyleBackColor = true;
+            this._btnAdd.Click += new System.EventHandler(this._btnAdd_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(969, 735);
+            this.Controls.Add(this._btnAdd);
+            this.Controls.Add(this._lblListBoxMessages);
+            this.Controls.Add(this._lstbMessages);
+            this.Controls.Add(this._txtbMessage);
+            this.Controls.Add(this._lblMessage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Dashboard";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label _lblMessage;
+        private System.Windows.Forms.TextBox _txtbMessage;
+        private System.Windows.Forms.ListBox _lstbMessages;
+        private System.Windows.Forms.Label _lblListBoxMessages;
+        private System.Windows.Forms.Button _btnAdd;
     }
 }
 
--- MessageWall/Dashboard.cs
+++ MessageWall/Dashboard.cs
@@ -12,9 +12,33 @@ namespace MessageWall
 {
     public partial class Dashboard : Form
     {
+        BindingList<string> messages = new BindingList<string>();
+
         public Dashboard()
         {
             InitializeComponent();
+            WireUpLists();
+        }
+
+        private void WireUpLists()
+        {
+            _lstbMessages.DataSource = messages;
+            //_lstbMessages.DisplayMember = nameof(Dashboard.Text);
+        }
+
+        private void _btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_txtbMessage.Text))
+            {
+                MessageBox.Show("Please enter a message.", "Blank Message Field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                messages.Add(_txtbMessage.Text);
+                _txtbMessage.Text = "";
+            }
+
+            _txtbMessage.Focus();
         }
     }
 }
